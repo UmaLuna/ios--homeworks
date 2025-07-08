75b5c0e75c8e2ab86e9cff9391036b02b5652b06
Uma <liasuma22@gmail.com>
2025-07-08 10:34:16 +0500

    Profile

---
+++ Navigation/Profile/PostTableViewCell.swift
@@ -0,0 +1,107 @@
+import UIKit
+
+final class PostTableViewCell: UITableViewCell {
+
+    private let authorLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        label.textColor = .black
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .systemGray
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let viewsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .regular)
+        label.textColor = .black
+        label.textAlignment = .right
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let bottomStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .equalSpacing
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupSubviews()
+        setupConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupSubviews()
+        setupConstraints()
+    }
+
+    private func setupSubviews() {
+        contentView.addSubview(authorLabel)
+        contentView.addSubview(postImageView)
+        contentView.addSubview(descriptionLabel)
+
+        bottomStackView.addArrangedSubview(likesLabel)
+        bottomStackView.addArrangedSubview(viewsLabel)
+        contentView.addSubview(bottomStackView)
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            authorLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+            authorLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            authorLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+
+            postImageView.topAnchor.constraint(equalTo: authorLabel.bottomAnchor, constant: 16),
+            postImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            postImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            postImageView.heightAnchor.constraint(equalTo: postImageView.widthAnchor), // квадрат
+
+            descriptionLabel.topAnchor.constraint(equalTo: postImageView.bottomAnchor, constant: 16),
+            descriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+
+            bottomStackView.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 16),
+            bottomStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            bottomStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            bottomStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        ])
+    }
+
+    func configure(with post: Post) {
+            authorLabel.text = post.author
+            postImageView.image = UIImage(named: post.image)
+            descriptionLabel.text = post.description
+            likesLabel.text = "Likes: \(post.likes)"
+            viewsLabel.text = "Views: \(post.views)"
+        }
+    }
